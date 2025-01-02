c19186dc6de0e8b6847f2cd39e646e2b1b237aa9
James Lochhead <james@lochhead.me>
2025-01-02 20:53:26 +0000

    feat: multiple features
    * A few additions to init.lua
    * Run Stylua
    * Removed some redunant keymaps

--- dotfiles/.config/nvim/init.lua
+++ dotfiles/.config/nvim/init.lua
@@ -1,27 +1,26 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 vim.opt.relativenumber = true
 vim.opt.number = true
 vim.opt.termguicolors = false
-vim.cmd.colorscheme 'vim'
+vim.cmd.colorscheme("vim")
 vim.opt.list = true
-vim.opt.spelllang = 'en_gb'
+vim.opt.spelllang = "en_gb"
 vim.g.have_nerd_font = true
 vim.opt.colorcolumn = "80,100"
+vim.opt.signcolumn = "yes"
 vim.opt.wrap = false
-vim.opt.listchars:append {
+vim.opt.listchars:append({
 	tab = "| ",
 	multispace = space,
 	lead = space,
-	trail = '·',
-	nbsp = '␣'
-}
+	trail = "·",
+	nbsp = "␣",
+})
 
-if package.loaded["lspconfig"] then
-	require'lspconfig'.ruff.setup{}
-	require'lspconfig'.marksman.setup{}
-	require'lspconfig'.terraform_lsp.setup{}
-end
+require("lspconfig").ruff.setup({})
+require("lspconfig").marksman.setup({})
+require("lspconfig").terraform_lsp.setup({})
 
 require("keymaps")
 require("autocmds")
--- dotfiles/.config/nvim/lua/autocmds.lua
+++ dotfiles/.config/nvim/lua/autocmds.lua
@@ -8,12 +8,12 @@ vim.api.nvim_create_autocmd("InsertEnter", {
 	callback = function()
 		vim.opt.listchars.trail = nil
 		vim.api.nvim_set_hl(0, "TrailingWhitespace", { link = "Whitespace" })
-	end
+	end,
 })
 
 vim.api.nvim_create_autocmd("InsertLeave", {
 	callback = function()
 		vim.opt.listchars.trail = space
 		vim.api.nvim_set_hl(0, "TrailingWhitespace", { link = "Error" })
-	end
+	end,
 })
