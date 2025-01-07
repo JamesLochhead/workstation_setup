df3afa123230082e5128f53d35c9adcacabed90f
James Lochhead <james@lochhead.me>
2025-01-07 01:49:18 +0000

    feat: add fzf-git

--- dotfiles/.bashrc.d/key-bindings.sh
+++ dotfiles/.bashrc.d/key-bindings.sh
@@ -6,4 +6,8 @@ if command -v fzf &>/dev/null &&
 	[[ -f "$(brew --prefix)/opt/fzf/shell/key-bindings.bash" ]] &&
 	[[ -L "$HOME/.homebrew" ]]; then
 	source "$(brew --prefix)/opt/fzf/shell/key-bindings.bash"
+
+	if [[ -f "$HOME/.local/share/fzf/fzf.git.sh" ]]; then
+		source "$HOME/.local/share/fzf/fzf.git.sh"
+	fi
 fi
