e60a5eca7822bab82fa625e375ab12fcc963fe31
James Lochhead <james@lochhead.me>
2025-01-12 15:33:33 +0000

    style: add line breaks to long lines

--- dotfiles/.bashrc.d/functions.sh
+++ dotfiles/.bashrc.d/functions.sh
@@ -4,7 +4,8 @@ open_file_fzf_neovim() {
 	local OLDIFS=$IFS
 	IFS=
 	local the_files=""
-	readarray -d '' files < <(find -maxdepth 4 -not -path "*/.*" -type f -print0 2>/dev/null)
+	readarray -d '' files < <(find -maxdepth 4 -not -path "*/.*" \
+		-type f -print0 2>/dev/null)
 	if [[ ${#files[@]} -eq 0 ]]; then
 		echo "No files found"
 		return
@@ -13,7 +14,10 @@ open_file_fzf_neovim() {
 			the_files+="$a_file"
 			the_files+=$'\n'
 		done
-		local chosen_file="$(echo $the_files | fzf -0 -1 --tiebreak=end --preview='less {}' --layout=reverse --bind=shift-up:preview-page-up,shift-down:preview-page-down)"
+		local chosen_file="$(echo $the_files |
+			fzf -0 -1 --tiebreak=end --preview='less {}' \
+				--layout=reverse \
+				--bind=shift-up:preview-page-up,shift-down:preview-page-down)"
 		if [[ -f "$chosen_file" ]]; then
 			nvim "$chosen_file"
 		else
@@ -32,7 +36,8 @@ preview_note_fzf_less() {
 	local OLDIFS=$IFS
 	IFS=
 	local the_files=""
-	readarray -d '' files < <(find -L "$HOME/Notes" -maxdepth 4 -not -path "*/.*" -name "*.md" -type f -print0 2>/dev/null)
+	readarray -d '' files < <(find -L "$HOME/Notes" -maxdepth 4 -not \
+		-path "*/.*" -name "*.md" -type f -print0 2>/dev/null)
 	if [[ ${#files[@]} -eq 0 ]]; then
 		echo "No files found"
 		return
@@ -41,7 +46,9 @@ preview_note_fzf_less() {
 			the_files+="$a_file"
 			the_files+=$'\n'
 		done
-		local chosen_file="$(echo $the_files | fzf -0 -1 --tiebreak=end --preview='less {}' --layout=reverse --bind=shift-up:preview-page-up,shift-down:preview-page-down)"
+		local chosen_file="$(echo $the_files |
+			fzf -0 -1 --tiebreak=end --preview='less {}' \
+				--layout=reverse --bind=shift-up:preview-page-up,shift-down:preview-page-down)"
 		if [[ -f "$chosen_file" ]]; then
 			less "$chosen_file"
 		else
@@ -60,7 +67,8 @@ open_fzf_macos() {
 	local OLDIFS=$IFS
 	IFS=
 	local the_files=""
-	readarray -d '' files < <(find -maxdepth 4 -not -path "*/.*" -print0 2>/dev/null)
+	readarray -d '' files < <(find -maxdepth 4 -not \
+		-path "*/.*" -print0 2>/dev/null)
 	if [[ ${#files[@]} -eq 0 ]]; then
 		echo "No files found"
 		return
@@ -69,7 +77,10 @@ open_fzf_macos() {
 			the_files+="$a_file"
 			the_files+=$'\n'
 		done
-		local chosen_file="$(echo $the_files | fzf -0 -1 --tiebreak=end --preview='less {}' --layout=reverse --bind=shift-up:preview-page-up,shift-down:preview-page-down)"
+		local chosen_file="$(echo $the_files |
+			fzf -0 -1 --tiebreak=end --preview='less {}' \
+				--layout=reverse \
+				--bind=shift-up:preview-page-up,shift-down:preview-page-down)"
 		if [[ -e "$chosen_file" ]]; then
 			open "$chosen_file"
 		else
